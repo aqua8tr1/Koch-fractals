d13081b82e537ce93d3e5b625e9d1b93dc28d7d2
aqua8tr1 <dashfadiman@gmail.com>
2024-06-06 09:36:33 -0700

    Add files via upload
    this 'extends' or uses Koch Generator! make sure you have them both!

---
+++ KochLine.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class KochLine : KochGenerator
+{
+LineRenderer _lineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.enabled = true;
+        _lineRenderer.useWorldSpace = false;
+        _lineRenderer.loop = true;
+        _lineRenderer.positionCount = _position.Length;
+        _lineRenderer.SetPositions(_position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

a6ef4c2d378e4f35a7952af9f879b7186cf52b15
aqua8tr1 <dashfadiman@gmail.com>
2024-06-07 08:51:06 -0700

    Update KochGenerator.cs

--- KochGenerator.cs
+++ KochGenerator.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class KochGenerator : MonoBehaviour
 {
-
+protected enum _axis
+{
+XAxis,
+YAxis,
+ZAxis
+};
+[SerializeField]
+protected _axis axis = new _axis();
 
 protected enum _initiator{
 Triangle,
@@ -16,28 +23,47 @@ Octagon
 };
 [SerializeField]
 protected _initiator initiator = new _initiator(); 
+[SerializeField]
+protected AnimationCurve _generator;
 
 protected int _initiatorPointAmount;
 private Vector3[] _initiatorPoint;
 private Vector3 _rotateVector;
 private Vector3 _rotateAxis;
+private float _initialRotation;
 [SerializeField]
 protected float _initiatorSize;
+
+protected Vector3[] _position;
+
+private void Awake()
+{
+GetInitiatorPoints();   
+_position = new Vector3[_initiatorPointAmount + 1];
+
+
+_rotateVector = Quaternion.AngleAxis(_initialRotation, _rotateAxis) * _rotateVector;
+for(int i = 0; i < _initiatorPointAmount; i++){
+_position[i] =  _rotateVector *  _initiatorSize;
+_rotateVector = Quaternion.AngleAxis(360 / _initiatorPointAmount, _rotateAxis) * _rotateVector;
+}
+_position[_initiatorPointAmount] = _position[0];
+
+}
 
 
 private void OnDrawGizmos(){
 GetInitiatorPoints();   
 _initiatorPoint = new Vector3[_initiatorPointAmount];
 
-_rotateVector = new Vector3(0, 0, 1);
-_rotateAxis = new Vector3(0, 1, 0);
 
+_rotateVector = Quaternion.AngleAxis(_initialRotation, _rotateAxis) * _rotateVector;
 for(int i = 0; i < _initiatorPointAmount; i++){
 _initiatorPoint[i] =  _rotateVector *  _initiatorSize;
 _rotateVector = Quaternion.AngleAxis(360 / _initiatorPointAmount, _rotateAxis) * _rotateVector;
 }//END 1st FOR LOOP
 
-/*~~~~~BUFFER~~~~~ */
+/*~~~~~BUFFER~~~~~*/
 
 for(int i = 0; i < _initiatorPointAmount; i++)
 {
@@ -59,26 +85,53 @@ private void GetInitiatorPoints(){
     switch(initiator){
         case _initiator.Triangle:
         _initiatorPointAmount = 3;
+        _initialRotation = 0;
             break;
          case _initiator.Square:
          _initiatorPointAmount = 4;
+         _initialRotation = 45;
             break;
          case _initiator.Pentagon:
          _initiatorPointAmount = 5;
+         _initialRotation = 36;
             break;
          case _initiator.Hexagon:
          _initiatorPointAmount = 6;
+         _initialRotation = 30;
             break;
          case _initiator.Hepagon:
          _initiatorPointAmount = 7;
+         _initialRotation = 25.71428f;
             break;
          case _initiator.Octagon:
          _initiatorPointAmount = 8;
+         _initialRotation = 22.5f;
             break;
             default: 
-_initiatorPointAmount = 3;
+      _initiatorPointAmount = 3;
+      _initialRotation = 0;
                 break;
     };//End switch(COME BACK TO LATER TO DEFINE WHAT A SWITCH IS!!!)
+
+    switch(axis)
+    {
+      case _axis.XAxis:
+_rotateVector = new Vector3(1, 0, 0);
+_rotateAxis = new Vector3(0, 0, 1);
+      break;
+      case _axis.YAxis:
+   _rotateVector = new Vector3(0, 1, 0);
+   _rotateAxis = new Vector3(1, 0, 0);
+      break;
+      case _axis.ZAxis:
+_rotateVector = new Vector3(0, 0, 1);
+_rotateAxis = new Vector3(0, 1, 0);
+      break;
+      default: 
+   _rotateVector = new Vector3(0, 1, 0);
+   _rotateAxis = new Vector3(1, 0, 0);
+      break;
+    };
 } //end GetInitiatorPoints
 
 

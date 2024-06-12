9f3fd6da13012e23734352bbeaddebb5d9be2ef9
aqua8tr1 <dashfadiman@gmail.com>
2024-06-12 09:31:05 -0700

    Update KochLine.cs

--- KochLine.cs
+++ KochLine.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 public class KochLine : KochGenerator
 {
 LineRenderer _lineRenderer;
-
+public float _lerpAmount;
+Vector3[] _lerpPosition;
+public float _generateMultiplier;
     // Start is called before the first frame update
     void Start()
     {
         _lineRenderer = GetComponent<LineRenderer>();
-        _lineRenderer.enabled = true;
+        _lineRenderer.enabled = true; //with .enabled, .useWorldSpace, and .loop we are setting the values we want at start(the first frame) so they stay like that regardless of what settings we fw in the line renderer!
         _lineRenderer.useWorldSpace = false;
         _lineRenderer.loop = true;
         _lineRenderer.positionCount = _position.Length;
@@ -21,6 +23,13 @@ LineRenderer _lineRenderer;
     // Update is called once per frame
     void Update()
     {
-        
+        if(input.GetKeyUp(KeyCode.O))
+        {
+            KochGenerate(_targetPosition, true, _generateMultiplier);
+        }
+        if(input.GetKeyUp(KeyCode.I))
+        {
+            KochGenerate(_targetPosition, true, _generateMultiplier);
+        }
     }
 }

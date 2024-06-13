457138c3426cdfb0167c11319760af38819a9604
aqua8tr1 <dashfadiman@gmail.com>
2024-06-13 09:46:05 -0700

    Update KochLine.cs

--- KochLine.cs
+++ KochLine.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class KochLine : KochGenerator
 {
 LineRenderer _lineRenderer;
+[Range(0,1)]
 public float _lerpAmount;
 Vector3[] _lerpPosition;
 public float _generateMultiplier;
@@ -23,13 +24,42 @@ public float _generateMultiplier;
     // Update is called once per frame
     void Update()
     {
-        if(input.GetKeyUp(KeyCode.O))
+        if(_generationCount != 0)
         {
-            KochGenerate(_targetPosition, true, _generateMultiplier);
+            for(int i = 0; i < _position.Length; i++) 
+            {
+                _lerpPosition[i] = Vector3.Lerp(_position[i], _targetPosition[i], _lerpAmount);
+            }           
+            if(_useBezierCurves)
+            {
+                _bezierPosition = BezierCurve(_lerpPosition, _bezierVertexCount);
+                _lineRenderer.positionCount = _bezierPosition.Length;
+                _lineRenderer.SetPositions(_bezierPosition);
+            } 
+            else
+            {
+                _lineRenderer.positionCount = _lerpPosition.Length;
+                _lineRenderer.SetPositions(_lerpPosition);
+            }
+
+            
         }
-        if(input.GetKeyUp(KeyCode.I))
+
+        if(Input.GetKeyUp(KeyCode.O))
         {
             KochGenerate(_targetPosition, true, _generateMultiplier);
+            _lerpPosition = new Vector3[_position.Length];
+            _lineRenderer.positionCount = _position.Length;
+            _lineRenderer.SetPositions(_position);
+            _lerpAmount = 0; 
+        }
+        if(Input.GetKeyUp(KeyCode.I))
+        {
+            KochGenerate(_targetPosition, true, _generateMultiplier);
+            _lerpPosition = new Vector3[_position.Length];
+            _lineRenderer.positionCount = _position.Length;
+            _lineRenderer.SetPositions(_position);
+            _lerpAmount = 0; 
         }
     }
 }
